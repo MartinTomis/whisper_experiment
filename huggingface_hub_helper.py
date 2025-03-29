61e8a7e1a51fd05351b1180aa615043ab0827516
Martin Tomis <martin.tomis@hotmail.com>
2025-03-29 22:11:23 +0100

    Huggingface hub clean-up

--- huggingface_hub_helper.py
+++ huggingface_hub_helper.py
@@ -25,7 +25,7 @@ snapshot_download(
 )
 
 snapshot_download(
-    repo_id="openai/whisper-tiny",
-    local_dir="models/whisper-tiny",
+    repo_id="Helsinki-NLP/opus-mt-cs-en",
+    local_dir="models/Helsinki-NLP/opus-mt-cs-en",
     local_dir_use_symlinks=False  # optional: copies instead of symlinks
 )
